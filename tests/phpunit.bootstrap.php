9455816d0e95907ade1ac5c57518641696acaba6
kafoso <soefritz@gmail.com>
2022-03-16 09:40:26 +0100

    Increased test memory limit til 512M

--- tests/phpunit.bootstrap.php
+++ tests/phpunit.bootstrap.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 require dirname(__DIR__) . '/script/bootstrap.php';
 
 define('TEST_ROOT_PATH', __DIR__);
-ini_set('memory_limit', '256M');
+ini_set('memory_limit', '512M');
