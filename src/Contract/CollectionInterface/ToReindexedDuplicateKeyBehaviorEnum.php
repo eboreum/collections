d44219d67aefb758277b6be5709a82f0d06ecad6
kafoso <soefritz@gmail.com>
2022-06-27 09:59:47 +0200

    Improved wording

--- src/Contract/CollectionInterface/ToReindexedDuplicateKeyBehaviorEnum.php
+++ src/Contract/CollectionInterface/ToReindexedDuplicateKeyBehaviorEnum.php
@@ -12,12 +12,14 @@ enum ToReindexedDuplicateKeyBehaviorEnum
     case throw_exception;
 
     /**
-     * Will use the first element for a given, duplicate key. The rest will be discarded.
+     * Will use the first element for a given, duplicate key. The rest, where this duplicate key is produced, will be
+     * discarded.
      */
     case use_first_element;
 
     /**
-     * Will use the last element for a given, duplicate key. All prior elements will be discarded.
+     * Will use the last element for a given, duplicate key. All prior elements, where this duplicate key is produced,
+     * will be discarded.
      */
     case use_last_element;
 }
